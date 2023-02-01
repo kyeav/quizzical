060dc74a9a97f29b223a52ef1fe680a53e28b89b
Kylie <kylieavie@gmail.com>
2023-02-01 22:32:51 +0800

    unchecked answers logic complete

--- src/components/Qna.jsx
+++ src/components/Qna.jsx
@@ -1,15 +1,43 @@
-export default function Qna() {
+import { useState } from "react";
+
+export default function Qna(props) {
+  const [selectedAns, setSelectedAns] = useState("");
+
+  const handleClick = (e) => {
+    setSelectedAns(e.target.innerHTML);
+  };
+
+  const uncheckedAns = (answer) => {
+    if (selectedAns === answer) {
+      return "selected";
+    }
+    return "unselected";
+  };
+
+  console.log(selectedAns)
+
   return (
     <section id="qna">
-      <h1>
-        Which best selling toy of 1983 caused hysteria, resulting in riots
-        breaking in stores?
-      </h1>
+      <h1>{props.qn}</h1>
       <div className="qna__btns">
-        <button>Adios</button>
-        <button>Hola</button>
-        <button>Au Revoir</button>
-        <button>Cabbage patch kids</button>
+        <button
+          onClick={handleClick}
+          className={
+            props.checked
+              ? checkedAns(props.correctAns)
+              : uncheckedAns(props.correctAns)
+          }
+        >
+          {props.correctAns}
+        </button>
+        {props.incorrectAns.map((ans) => (
+          <button
+            onClick={handleClick}
+            className={props.checked ? checkedAns(ans) : uncheckedAns(ans)}
+          >
+            {ans}
+          </button>
+        ))}
       </div>
       <div className="qna__line"></div>
     </section>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,54 @@
+import { useEffect, useState } from "react";
 import Qna from "./Qna";
+import { nanoid } from "nanoid";
 
-export default function Quizz() {
+export default function Quiz() {
+  const [quiz, setQuiz] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    const getQuiz = async () => {
+      const res = await fetch(
+        "https://opentdb.com/api.php?amount=5&type=multiple"
+      );
+      const data = await res.json();
+      setQuiz(data.results);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    };
+    getQuiz();
+  }, []);
+
+  const handleCheckAnswers = () => {
+    setChecked(true);
+  };
+
+  console.log(checked)
+
   return (
     <section id="quiz">
       <div className="quiz">
-        <Qna />
-        <Qna />
-        <Qna />
-        <Qna />
-        <Qna />
+        {loading ? (
+          <h1 className="quiz__h1">Loading...</h1>
+        ) : (
+          quiz.map((qna) => (
+            <Qna
+              key={nanoid()}
+              qn={qna.question}
+              correctAns={qna.correct_answer}
+              incorrectAns={qna.incorrect_answers}
+              checked={checked}
+            />
+          ))
+        )}
       </div>
 
-      <button className="quiz__btn">Check answers</button>
+      <button onClick={handleCheckAnswers} className="quiz__btn">
+        Check answers
+      </button>
     </section>
   );
 }
