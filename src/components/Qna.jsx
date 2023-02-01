497a94eb801cc639efac47a52d43a6d021d7bf52
Kylie <kylieavie@gmail.com>
2023-02-01 15:24:44 +0800

    basic css done

--- src/components/Qna.jsx
+++ src/components/Qna.jsx
@@ -1,12 +1,15 @@
 export default function Qna() {
   return (
     <section id="qna">
-      <h3>What is the hottest planet in our solar system?</h3>
+      <h1>
+        Which best selling toy of 1983 caused hysteria, resulting in riots
+        breaking in stores?
+      </h1>
       <div className="qna__btns">
         <button>Adios</button>
         <button>Hola</button>
         <button>Au Revoir</button>
-        <button>Salir</button>
+        <button>Cabbage patch kids</button>
       </div>
       <div className="qna__line"></div>
     </section>
